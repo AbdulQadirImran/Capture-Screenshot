b54eed1fe2af70ca5eeb24c81233a8b13f7c1ff2
AbdulQadirImran <160231143+AbdulQadirImran@users.noreply.github.com>
2024-04-14 13:44:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const screenshotBtn = document.querySelector("#src-btn"),
     screenshotPreview = document.querySelector(".src-preview"),
     closeBtn = screenshotPreview.querySelector("#close-btn");
 
+
 const captureScreen = async() => {
     try {
         // asking permission to use a media input to record current tab
